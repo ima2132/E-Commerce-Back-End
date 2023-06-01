496f461a79110a56d7df5e8a02f8c30f456fcb71
ima2132 <ima2132@columbia.edu>
2023-05-31 23:22:06 -0400

    added to schema.sql; worked on insomnia

--- db/schema.sql
+++ db/schema.sql
@@ -4,3 +4,40 @@ DROP DATABASE IF EXISTS ecommerce_db;
 -- CREATE DATABASE
 CREATE DATABASE ecommerce_db;
 
+-- CREATE TABLE Category
+CREATE TABLE Category (
+  id INT NOT NULL AUTO_INCREMENT,
+  category_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- CREATE TABLE Product 
+CREATE TABLE Product (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_name VARCHAR(255) NOT NULL,
+  price DECIMAL(10, 2) NOT NULL,
+  stock INT NOT NULL DEFAULT 10,
+  category_id INT NOT NULL,
+  PRIMARY KEY (id),
+  FOREIGN KEY (category_id) REFERENCES Category(id) ON DELETE CASCADE
+);
+
+
+-- CREATE TABLE Tag
+CREATE TABLE Tag (
+  id INT NOT NULL AUTO_INCREMENT,
+  tag_name VARCHAR(255) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+
+-- CREATE TABLE ProductTag
+CREATE TABLE Product_Tag (
+  id INT NOT NULL AUTO_INCREMENT,
+  product_id INT,
+  tag_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (product_id) REFERENCES Product(id) ON DELETE CASCADE,
+  FOREIGN KEY (tag_id) REFERENCES Tag(id) ON DELETE CASCADE
+);
